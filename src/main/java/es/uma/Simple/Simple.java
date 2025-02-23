506d0b58ee41bb3483e11853b6212da94bf75417
a-coman <141495654+a-coman@users.noreply.github.com>
2025-02-23 13:21:02 +0100

    Rename to gen

--- src/main/java/es/uma/Simple/Simple.java
+++ src/main/java/es/uma/Simple/Simple.java
@@ -12,12 +12,12 @@ public class Simple {
         String exampleSOIL = Utils.readFile(experiment.examplePath);
 
         String response = simple.chat(modelUML, exampleSOIL);
-        Utils.saveFile(response, experiment.instancePath, "instance1.soil");
+        Utils.saveFile(response, experiment.instancePath, "gen1.soil");
         Utils.saveFile(response, experiment.instancePath, "output.md");
 
         for (int i = 2; i <= 5; i++) {
             response = simple.chat("Let's continue creating more instances");
-            Utils.saveFile(response, experiment.instancePath, "instance" + i + ".soil");
+            Utils.saveFile(response, experiment.instancePath, "gen" + i + ".soil");
             Utils.saveFile(response, experiment.instancePath, "output.md");
         }
     }
