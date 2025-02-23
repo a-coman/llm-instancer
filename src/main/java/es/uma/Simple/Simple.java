738a9dc0102898400ae3c47cbad9c2fb12a8e012
a-coman <141495654+a-coman@users.noreply.github.com>
2025-02-23 12:18:26 +0100

    Update ISimple with loop

--- src/main/java/es/uma/Simple/Simple.java
+++ src/main/java/es/uma/Simple/Simple.java
@@ -12,8 +12,13 @@ public class Simple {
         String exampleSOIL = Utils.readFile(experiment.examplePath);
 
         String response = simple.chat(modelUML, exampleSOIL);
-
-        Utils.saveFile(response, experiment.instancePath, "instance.soil");
+        Utils.saveFile(response, experiment.instancePath, "instance1.soil");
         Utils.saveFile(response, experiment.instancePath, "output.md");
+
+        for (int i = 2; i <= 5; i++) {
+            response = simple.chat("Let's continue creating more instances");
+            Utils.saveFile(response, experiment.instancePath, "instance" + i + ".soil");
+            Utils.saveFile(response, experiment.instancePath, "output.md");
+        }
     }
 }
