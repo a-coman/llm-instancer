7e4d0556a8efe825f887e0dd788dd6c4bbdf8c49
a-coman <141495654+a-coman@users.noreply.github.com>
2025-02-28 15:59:19 +0100

    Enable request and response logging for Llms

--- src/main/java/es/uma/Llms.java
+++ src/main/java/es/uma/Llms.java
@@ -1,5 +1,6 @@
 package es.uma;
 
+import java.time.Duration;
 import java.util.List;
 
 import dev.langchain4j.memory.ChatMemory;
@@ -27,12 +28,16 @@ public class Llms {
                 return OpenAiChatModel.builder()
                     .apiKey(System.getenv("OPENAI_KEY"))
                     .modelName("gpt-4o")
+                    .logRequests(true)
+                    .logResponses(true)
                     .listeners(List.of(new Listener()))
                     .build();
             case GPT_O3_MINI:
                 return OpenAiChatModel.builder()
                     .apiKey(System.getenv("OPENAI_KEY"))
                     .modelName("o3-mini-2025-01-31")
+                    .logRequests(true)
+                    .logResponses(true)
                     .listeners(List.of(new Listener()))
                     .build();
             case DEEPSEEK_V3:
@@ -40,31 +45,42 @@ public class Llms {
                     .baseUrl("https://api.deepseek.com")
                     .apiKey(System.getenv("DEEPSEEK_KEY"))
                     .modelName("deepseek-chat")
+                    .logRequests(true)
+                    .logResponses(true)
                     .listeners(List.of(new Listener()))
+                    .maxRetries(10)
+                    .timeout(Duration.ofSeconds(180))
                     .build();
             case DEEPSEEK_R3:
                 return OpenAiChatModel.builder()
                     .baseUrl("https://api.deepseek.com")
                     .apiKey(System.getenv("DEEPSEEK_KEY"))
                     .modelName("deepseek-reasoner")
+                    .logRequests(true)
+                    .logResponses(true)
                     .listeners(List.of(new Listener()))
+                    .maxRetries(10)
+                    .timeout(Duration.ofSeconds(180))
                     .build();
             case GEMINI_2_PRO:
                 return GoogleAiGeminiChatModel.builder()
                     .apiKey(System.getenv("GEMINI_KEY"))
                     .modelName("gemini-2.0-pro-exp-02-05")
+                    .logRequestsAndResponses(true)
                     .listeners(List.of(new Listener()))
                     .build();
             case GEMINI_2_REASONER:
                 return GoogleAiGeminiChatModel.builder()
                     .apiKey(System.getenv("GEMINI_KEY"))
                     .modelName("gemini-2.0-flash-thinking-exp-01-21")
+                    .logRequestsAndResponses(true)
                     .listeners(List.of(new Listener()))
                     .build();
             case GEMINI_2_FLASH_LITE:
                 return GoogleAiGeminiChatModel.builder()
                     .apiKey(System.getenv("GEMINI_KEY"))
                     .modelName("gemini-2.0-flash-lite-preview-02-05")
+                    .logRequestsAndResponses(true)
                     .listeners(List.of(new Listener()))
                     .build();
             default:
